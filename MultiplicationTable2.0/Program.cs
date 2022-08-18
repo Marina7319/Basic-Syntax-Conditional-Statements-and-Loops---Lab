7efa5481a0fe184c77956ed013793ca1caa5f9e6
Marina7319 <marina-markova@abv.bg>
2022-08-18 11:54:42 +0300

    commit Multiplication Table 2.0

---
+++ MultiplicationTable2.0/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace MultiplicationTable2._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int theInteger = int.Parse(Console.ReadLine());
+            int times = int.Parse(Console.ReadLine());
+            do
+            {
+                Console.WriteLine($"{theInteger} X {times} = {theInteger * times}");
+                times++;
+            } while (times <= 10);
+        }
+    }
+}

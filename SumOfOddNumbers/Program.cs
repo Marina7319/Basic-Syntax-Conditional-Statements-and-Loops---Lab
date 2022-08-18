087a166099baa5b3e3220d1405c5bfaece1e5807
Marina7319 <marina-markova@abv.bg>
2022-08-18 11:55:45 +0300

    commit Sum of Odd Numbers

---
+++ SumOfOddNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SumOfOddNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfOddNumbers = int.Parse(Console.ReadLine());
+            int sum = 0;
+            for (int i = 0; i < countOfOddNumbers; i++)
+            {
+                int currentOddNumber = 1 + (i * 2);
+                Console.WriteLine(currentOddNumber);
+                sum += currentOddNumber;
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
